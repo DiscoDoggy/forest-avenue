02a93bda657b7ceef89976b56cf9cca048473451
DiscoDoggy <akaattarilim@gmail.com>
2026-03-16 12:39:38 -0700

    debugged infinite GPS trigger and integrated button start, pause, stop state actions to actually respond on map

--- forest-ave/app/components/tripMap.tsx
+++ forest-ave/app/components/tripMap.tsx
@@ -1,45 +1,67 @@
 import { useState, useEffect, useRef } from 'react';
 import {View, StyleSheet, Platform, PermissionsAndroid} from 'react-native';
-import Mapbox, {MapView, LocationPuck, Camera} from "@rnmapbox/maps";
+import Mapbox, {MapView, LocationPuck, Camera, ShapeSource, LineLayer, LineLayerStyle} from "@rnmapbox/maps";
 import * as Location from 'expo-location';
 
 
 Mapbox.setAccessToken("pk.eyJ1IjoidGhlZmxpZ2h0bGVzc2JpcmQiLCJhIjoiY21tazN3MTQzMWdybzJ3b2M4dHF0Y3JrZSJ9.vwuF1cIXhLfvYU-p1PL7Hw");
 Mapbox.setTelemetryEnabled(false);
 
-export default function TripMap(){ 
+interface TripMapProps {
+    isTripStarted: boolean;
+    isTripPaused: boolean;
+    isTripStopped: boolean;
+
+}
+
+export default function TripMap({isTripStarted, isTripPaused, isTripStopped}: TripMapProps){ 
 
     const [getLocation, setLocation] = useState<Location.LocationObject | null>(null);
     const camera = useRef<Camera>(null);
-    useEffect(() => {( async () => {
+    const [getTripCoords, setTripCoords]= useState<number[][]>([]);
+
+    useEffect(() => {
         let locationSub: Location.LocationSubscription | null=null;
+        console.log(`button states: tripStarted: ${isTripStarted} tripPaused: ${isTripPaused} tripStopped: ${isTripStopped}`);
 
-        const hasPerms = await requestLocationPermission();
-        if(!hasPerms) { 
-            console.log('Permission to access location was denied');
-            return;
-        } 
-        const subscription = await Location.watchPositionAsync({
-                accuracy: Location.Accuracy.High,
-                distanceInterval: 5,
-                timeInterval: 5000
-        }, (location) => {
-            setLocation(location)
-            camera.current?.setCamera({
-                centerCoordinate: [location.coords.longitude, location.coords.latitude]
-            });
-            console.log('New location update: ' + location.coords.latitude + ', ' + location.coords.longitude);
-        });
+        (async () => {
 
-        locationSub = subscription;
+            if(isTripStarted && !isTripStopped && !isTripPaused) {
+                const hasPerms = await requestLocationPermission();
+                if(!hasPerms) { 
+                    console.log('Permission to access location was denied');
+                    return;
+                }
+
+                const subscription = await Location.watchPositionAsync({
+                    accuracy: Location.Accuracy.High,
+                    distanceInterval: 1,
+                    timeInterval: 5000
+                }, (location) => {
+                    setLocation(location)
+                    camera.current?.setCamera({
+                        centerCoordinate: [location.coords.longitude, location.coords.latitude]
+                    });
+
+                    //update routing
+                //    setTripCoords([...getTripCoords, [location.coords.longitude, location.coords.latitude]]);
+                    setTripCoords(prev => [...prev, [location.coords.longitude, location.coords.latitude]])
+
+                    console.log('New location update: ' + location.coords.latitude + ', ' + location.coords.longitude);
+                });
+
+                locationSub = subscription;
+            }
+    })();
 
         return () => {
-            if(subscription) {
+            if(locationSub) {
                 locationSub.remove();
                 console.log("location tracking paused or stopped");
             }
-        } 
-    })()}, [])
+        };   
+
+    }, [isTripStarted, isTripPaused, isTripStopped])
 
     return (
         <View style={styles.map}>
@@ -55,13 +77,32 @@ export default function TripMap(){
 
                 <Camera 
                     ref={camera}
-                    zoomLevel={20.1} 
+                    zoomLevel={17.1} 
+                   
                 />
+
+                <ShapeSource id='line-source' shape={{
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'LineString',
+                        coordinates: getTripCoords
+                    }
+                }}>
+
+                    <LineLayer id='line-layer' style={lineLayerStyle} slot='middle'></LineLayer>
+
+                </ShapeSource>
 
             </MapView>
         </View>
     );
 }
+
+const lineLayerStyle: LineLayerStyle = {
+  lineColor: '#ff0000',
+  lineWidth: 6.0,
+};
 
 const styles = StyleSheet.create({
     map: {
@@ -91,3 +132,10 @@ const requestLocationPermission = async () => {
   }
   return true;
 };
+
+
+
+
+// we want to implement the button parts. stop saves coords maybe as GEOJSON file in storage or maybe just as plain text
+    // on stop we store in local storage and then save to online server if wifi
+    // on pause we unsubscribe from location events and on resume we resubscribe
