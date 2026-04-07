bf0cffe5181702e40a92558dc679fcaaf5200822
DiscoDoggy <akaattarilim@gmail.com>
2026-04-06 22:14:16 -0700

    installed jest for testing, wrote unit tests for obd pin decoding, debugged failing tests (not knowing join() leaves commas in)

--- forest-ave/constants/obdiiCommands.ts
+++ forest-ave/constants/obdiiCommands.ts
@@ -20,21 +20,21 @@ first two hexadecimals values repeat the OBD mode and the PIN that was queried.
 For example, for RPM indexing starting at [2] bypasses the mode hex and PID hex returned by OBD2  
 
 as an example, for RPM we may send 01 0C to the ELM327. In response we would get, 01 0C 27 10
-where 01 0C is the repeat of the mode and PIN we requested and 27 10 is the RPM.
+where 41 0C is the repeat of the mode and PIN we requested and 27 10 is the RPM.
 */
 
 export const OBDPIDS = {
     RPM: new OBDPIDCmd('RPM', '01 0C', (obdHexOutput) => {
         //OBD2 returns RPM in 4ths of an RPM so hex represents 4x the normal RPM
         const processedHex = processAndValidateObdIIOutputHex(obdHexOutput, 2);
-        const RPM = parseInt(processedHex.slice(2).join(), 16);
+        const RPM = parseInt(processedHex.slice(2).join(""), 16);
         return Math.round(RPM / 4);
     }),
 
     MAF: new OBDPIDCmd('MAF', '01 10', (obdHexOutput) => {
         const hexArr = processAndValidateObdIIOutputHex(obdHexOutput, 2);
-        const MAF = parseInt(hexArr.slice(2).join(), 16);
-        return Math.round(MAF / 100);
+        const MAF = parseInt(hexArr.slice(2).join(""), 16);
+        return MAF / 100;
     }),
 
     MAP: new OBDPIDCmd('MAP', '01 0B', (obdHexOutput) => {
